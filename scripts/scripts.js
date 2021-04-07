99eef73de1f991b92bc7f754f97f27c869a162b3
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-04-07 18:49:55 +0200

    Create script to calculate age. Update pdf with correct age

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,31 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+  dataNasc = '07/12/1984';
+  var dataAtual = new Date();
+  var anoAtual = dataAtual.getFullYear();
+  var anoNascParts = dataNasc.split('/');
+  var diaNasc =anoNascParts[0];
+  var mesNasc =anoNascParts[1];
+  var anoNasc =anoNascParts[2];
+  var idade = anoAtual - anoNasc;
+  var mesAtual = dataAtual.getMonth() + 1; 
+  //Se mes atual for menor que o nascimento, nao fez aniversario ainda;  
+  if(mesAtual < mesNasc){
+  idade--; 
+  } else {
+  //Se estiver no mes do nascimento, verificar o dia
+  if(mesAtual == mesNasc){ 
+  if(new Date().getDate() < diaNasc ){ 
+  //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
+  idade--; 
+  }
+  }
+  } 
+  document.getElementById("age").innerHTML = idade + " años"
+
+
+
+
+ 
